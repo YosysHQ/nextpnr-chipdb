f1986d173939082a5e8e860944df7e9086ff3d32
Miodrag Milanovic <mmicko@gmail.com>
2019-02-09 10:15:07 +0100

    Build ice40 chipdb based on icestorm from source tree

--- ice40/family.cmake
+++ ice40/family.cmake
@@ -1,26 +1,34 @@
 set(devices 384 1k 5k 8k)
 set(DB_PY ${CMAKE_CURRENT_SOURCE_DIR}/external/nextpnr/ice40/chipdb.py)
 
-set(ICEBOX_ROOT "/usr/local/share/icebox" CACHE STRING "icebox location root")
+set(ICEBOX_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/external/icestorm/icebox)
+
 file(MAKE_DIRECTORY ice40/chipdbs/)
 add_library(ice40_chipdb OBJECT ice40/chipdbs/)
 
 foreach (dev ${devices})
     if (dev EQUAL "5k")
         set(OPT_FAST "")
-        set(OPT_SLOW --slow ${ICEBOX_ROOT}/timings_up5k.txt)
+        set(OPT_SLOW --slow ${ICEBOX_ROOT}/../icefuzz/timings_up5k.txt)
     elseif(dev EQUAL "384")
         set(OPT_FAST "")
-        set(OPT_SLOW --slow ${ICEBOX_ROOT}/timings_lp384.txt)
+        set(OPT_SLOW --slow ${ICEBOX_ROOT}/../icefuzz/timings_lp384.txt)
     else()
-        set(OPT_FAST --fast ${ICEBOX_ROOT}/timings_hx${dev}.txt)
-        set(OPT_SLOW --slow ${ICEBOX_ROOT}/timings_lp${dev}.txt)
+        set(OPT_FAST --fast ${ICEBOX_ROOT}/../icefuzz/timings_hx${dev}.txt)
+        set(OPT_SLOW --slow ${ICEBOX_ROOT}/../icefuzz/timings_lp${dev}.txt)
     endif()
     set(DEV_TXT_DB ${ICEBOX_ROOT}/chipdb-${dev}.txt)
     set(DEV_CC_BBA_DB ${CMAKE_CURRENT_SOURCE_DIR}/ice40/chipdbs/chipdb-${dev}.bba)
     set(DEV_CC_DB ${CMAKE_CURRENT_SOURCE_DIR}/ice40/chipdbs/chipdb-${dev}.bin)
     set(DEV_CONSTIDS_INC ${CMAKE_CURRENT_SOURCE_DIR}/external/nextpnr/ice40/constids.inc)
     set(DEV_GFXH ${CMAKE_CURRENT_SOURCE_DIR}/external/nextpnr/ice40/gfx.h)
+    add_custom_command(OUTPUT ${ICEBOX_ROOT}/chipdb-${dev}.txt
+            COMMAND make chipdb-${dev}.txt
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/icestorm/icebox
+            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/external/icestorm/icebox/icebox.py 
+                    ${CMAKE_CURRENT_SOURCE_DIR}/external/icestorm/icebox/iceboxdb.py 
+                    ${CMAKE_CURRENT_SOURCE_DIR}/external/icestorm/icebox/icebox_chipdb.py
+    )
     add_custom_command(OUTPUT ${DEV_CC_BBA_DB}
             COMMAND ${PYTHON_EXECUTABLE} ${DB_PY} -p ${DEV_CONSTIDS_INC} -g ${DEV_GFXH} ${OPT_FAST} ${OPT_SLOW} ${DEV_TXT_DB} > ${DEV_CC_BBA_DB}
             DEPENDS ${DEV_CONSTIDS_INC} ${DEV_GFXH} ${DEV_TXT_DB} ${DB_PY}
