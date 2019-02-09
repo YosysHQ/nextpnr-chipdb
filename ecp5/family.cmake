2b42e5407f85bec72e1af80df21ebd7d2c6ebdd2
Miodrag Milanovic <mmicko@gmail.com>
2019-02-09 10:51:53 +0100

    Make ECP5 chipdb using files from source tree only

--- ecp5/family.cmake
+++ ecp5/family.cmake
@@ -1,16 +1,9 @@
 
 set(devices 25k 45k 85k)
 
-if (NOT DEFINED TRELLIS_ROOT)
-    message(STATUS "TRELLIS_ROOT not defined using -DTRELLIS_ROOT=/path/to/prjtrellis. Default to /usr/local/share/trellis")
-    set(TRELLIS_ROOT "/usr/local/share/trellis")
-endif()
+add_subdirectory(external/prjtrellis/libtrellis ${CMAKE_CURRENT_BINARY_DIR}/generated/libtrellis)
 
-file(GLOB found_pytrellis ${TRELLIS_ROOT}/libtrellis/pytrellis.*)
-
-if ("${found_pytrellis}" STREQUAL "")
-    message(FATAL_ERROR "failed to find pytrellis library in ${TRELLIS_ROOT}/libtrellis/")
-endif()
+set(TRELLIS_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/external/prjtrellis)
 
 set(DB_PY ${CMAKE_CURRENT_SOURCE_DIR}/external/nextpnr/ecp5/trellis_import.py)
 
@@ -18,9 +11,9 @@ file(MAKE_DIRECTORY ecp5/chipdbs/)
 add_library(ecp5_chipdb OBJECT ecp5/chipdbs/)
 
 if (CMAKE_HOST_WIN32)
-set(ENV_CMD ${CMAKE_COMMAND} -E env "PYTHONPATH=\"${TRELLIS_ROOT}/libtrellis\;${TRELLIS_ROOT}/util/common\;${TRELLIS_ROOT}/timing/util\"")
+set(ENV_CMD ${CMAKE_COMMAND} -E env "PYTHONPATH=\"${CMAKE_CURRENT_BINARY_DIR}/generated/libtrellis\;${TRELLIS_ROOT}/util/common\;${TRELLIS_ROOT}/timing/util\"")
 else()
-set(ENV_CMD ${CMAKE_COMMAND} -E env "PYTHONPATH=${TRELLIS_ROOT}/libtrellis:${TRELLIS_ROOT}/util/common:${TRELLIS_ROOT}/timing/util")
+set(ENV_CMD ${CMAKE_COMMAND} -E env "PYTHONPATH=${CMAKE_CURRENT_BINARY_DIR}/generated/libtrellis:${TRELLIS_ROOT}/util/common:${TRELLIS_ROOT}/timing/util")
 endif()
 
 foreach (dev ${devices})
@@ -29,7 +22,7 @@ foreach (dev ${devices})
     set(DEV_CONSTIDS_INC ${CMAKE_CURRENT_SOURCE_DIR}/external/nextpnr/ecp5/constids.inc)
     add_custom_command(OUTPUT ${DEV_CC_BBA_DB}
     COMMAND ${ENV_CMD} python3 ${DB_PY} -p ${DEV_CONSTIDS_INC} ${dev} > ${DEV_CC_BBA_DB}
-            DEPENDS ${DB_PY}
+            DEPENDS ${DB_PY} pytrellis
             )
     add_custom_command(OUTPUT ${DEV_CC_DB}
             COMMAND bbasm ${DEV_CC_BBA_DB} ${DEV_CC_DB}
